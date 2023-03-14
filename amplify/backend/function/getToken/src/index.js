0bbad065901ad085b77c13c252694d805928f7b2
Julie Souchet <90184650+SouchetJulie@users.noreply.github.com>
2023-03-14 14:36:51 +0100

    [lambda] Check if user exists

--- amplify/backend/function/getToken/src/index.js
+++ amplify/backend/function/getToken/src/index.js
@@ -1,15 +1,50 @@
-const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./app');
+const {DynamoDBClient, GetItemCommand} = require("@aws-sdk/client-dynamodb");
+const {DynamoDBDocumentClient, QueryCommand, ScanCommand} = require("@aws-sdk/lib-dynamodb");
 
-/**
- * @type {import('http').Server}
- */
-const server = awsServerlessExpress.createServer(app);
 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-exports.handler = (event, context) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
-  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
+exports.handler = async (event, context) => {
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+  const client = new DynamoDBClient({
+    region: process.env.REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    },
+  });
+  const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+  let response = {};
+
+  try {
+
+    // Check if the user already exists in DB
+    const allItems = await ddbDocClient.send(new ScanCommand({
+      TableName: process.env.STORAGE_AWSJSDB_NAME,
+    }))
+    const body = JSON.parse(event.body);
+    const user = allItems.Items.find(item => item.lastname === body.lastname);
+
+    if (user) {
+      response = {
+        statusCode: 400,
+        body: JSON.stringify({error: "User already exists"})
+      };
+    } else {
+      response = {
+        statusCode: 200,
+        body: "TODO create new user & token"
+      };
+    }
+  } catch (e) {
+    console.log(e);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({error: e.message})
+    };
+  }
+
+  ddbDocClient.destroy();
+  client.destroy();
+
+  return response;
 };
