ace3e62a9b0d9860e146bc4c58209d445abdd57e
Julie Souchet <90184650+SouchetJulie@users.noreply.github.com>
2023-03-15 16:07:11 +0100

    Formatting

--- amplify/backend/function/sendData/src/index.js
+++ amplify/backend/function/sendData/src/index.js
@@ -3,12 +3,15 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 const {checkSecret, getSecret} = require('/opt/nodejs/utils')
+
 exports.handler = async (event) => {
   let response;
   try {
     await checkSecret('awsJsprofil', event.headers['x-api-key']);
     const userId = await getSecret(event.headers['x-user-token'], 'pk');
+
     const {db, data} = JSON.parse(event.body);
+
     response = {
       statusCode: 200,
       body: JSON.stringify({userId, db, data})
@@ -20,5 +23,6 @@ exports.handler = async (event) => {
       body: JSON.stringify({error: e.message})
     };
   }
+
   return response;
 };
