6c4a99d4104f083b76171a13822160fdb31bd260
Julie Souchet <90184650+SouchetJulie@users.noreply.github.com>
2023-03-15 15:50:11 +0100

    Fix sendData secret access

--- amplify/backend/function/sendData/src/index.js
+++ amplify/backend/function/sendData/src/index.js
@@ -3,19 +3,12 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 const {checkSecret, getSecret} = require('/opt/nodejs/utils')
-
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
 exports.handler = async (event) => {
   let response;
-
   try {
     await checkSecret('awsJsprofil', event.headers['x-api-key']);
     const userId = await getSecret(event.headers['x-user-token'], 'pk');
-
     const {db, data} = JSON.parse(event.body);
-
     response = {
       statusCode: 200,
       body: JSON.stringify({userId, db, data})
@@ -27,6 +20,5 @@ exports.handler = async (event) => {
       body: JSON.stringify({error: e.message})
     };
   }
-
   return response;
 };
